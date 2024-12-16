291bbcbb5dff98a8a416afaeda09c14be44d378f
Miguel Delgado <mdelgado.cab@gmail.com>
2024-12-15 19:58:52 -0600

    chore: added abstraction for logger

--- shared/logger/logger.go
+++ shared/logger/logger.go
@@ -1,22 +1,19 @@
 package logger
 
-// Info loguea un mensaje con nivel de información
+type Logger interface {
+	Info(fields map[string]interface{}, message string)
+	Warn(fields map[string]interface{}, message string)
+	Error(fields map[string]interface{}, message string)
+}
+
 func Info(log Logger, message string, fields map[string]interface{}) {
-	log.Info().
-		Fields(fields).
-		Msg(message)
+	log.Info(fields, message)
 }
 
-// Warn loguea un mensaje con nivel de advertencia
 func Warn(log Logger, message string, fields map[string]interface{}) {
-	log.Warn().
-		Fields(fields).
-		Msg(message)
+	log.Warn(fields, message)
 }
 
-// Error loguea un mensaje con nivel de error
 func Error(log Logger, message string, fields map[string]interface{}) {
-	log.Error().
-		Fields(fields).
-		Msg(message)
+	log.Error(fields, message)
 }
--- shared/logger/zero_log.go
+++ shared/logger/zero_log.go
@@ -10,12 +10,6 @@ type ZerologLogger struct {
 	logger zerolog.Logger
 }
 
-type Logger interface {
-	Info() *zerolog.Event
-	Warn() *zerolog.Event
-	Error() *zerolog.Event
-}
-
 // Nueva instancia de un ZerologLogger
 func NewZerologLogger() *ZerologLogger {
 	return &ZerologLogger{
@@ -23,15 +17,14 @@ func NewZerologLogger() *ZerologLogger {
 	}
 }
 
-// Métodos de la interfaz Logger para Zerolog
-func (l *ZerologLogger) Info() *zerolog.Event {
-	return l.logger.Info()
+func (l *ZerologLogger) Info(fields map[string]interface{}, message string) {
+	l.logger.Info().Fields(fields).Msg(message)
 }
 
-func (l *ZerologLogger) Warn() *zerolog.Event {
-	return l.logger.Warn()
+func (l *ZerologLogger) Warn(fields map[string]interface{}, message string) {
+	l.logger.Warn().Fields(fields).Msg(message)
 }
 
-func (l *ZerologLogger) Error() *zerolog.Event {
-	return l.logger.Error()
+func (l *ZerologLogger) Error(fields map[string]interface{}, message string) {
+	l.logger.Error().Fields(fields).Msg(message)
 }
